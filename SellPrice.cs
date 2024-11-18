af6e831c05930473f29dceb8555bc1547803271f
air1068 <slashangemon@gmail.com>
2024-11-18 06:06:30 -0600

    Add files via upload

---
+++ SellPrice.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace SellPrice;
+internal static class ModInfo {
+    internal const string Guid = "air1068.elin.sellprice";
+    internal const string Name = "SellPrice";
+    internal const string Version = "0.1.0";
+}
+
+[BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
+class SellPrice : BaseUnityPlugin {
+    private void Awake() {
+        var harmony = new Harmony(ModInfo.Guid);
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(Thing), nameof(Thing.WriteNote))]
+class Thing_WriteNote_Patch {
+    static void Postfix(Thing __instance, UINote n) {
+        n.AddText("Value: " + __instance.GetPrice(CurrencyType.Money, false, PriceType.Default, null).ToString(), FontColor.DontChange);
+    }
+}
